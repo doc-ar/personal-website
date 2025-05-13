c747b463cdd89022c69fcc541add547dcbe10dcb
AbdurRahman <weused@gmail.com>
2025-05-13 16:29:23 +0500

    Added Reset Button to Game

--- src/components/Games/Games.jsx
+++ src/components/Games/Games.jsx
@@ -3,21 +3,34 @@ import GameButton from "components/GameButton";
 import TicTacToe from "components/TicTacToe";
 import tictactoeIcon from "assets/tictactoe.svg";
 import two48Icon from "assets/2048.svg";
+import resetIcon from "assets/reset.svg";
+import { useState } from "react";
 
 export default function Games(props) {
+  const [resetKey, setResetKey] = useState(0);
+
+  function resetOnClick() {
+    setResetKey((prev) => prev + 1);
+  }
+
   return (
     <>
       <div className={`${styles.container} ${props.className || ""}`}>
-        <h1 className={styles.header}>Play Some Games</h1>
+        <h1 className={styles.header}>Play A Game</h1>
         <div className={styles.top_section}>
           <div className={styles.buttons_container}>
             <GameButton className={styles.game_button} icon={tictactoeIcon} />
             <GameButton className={styles.game_button} icon={two48Icon} />
           </div>
-          <div className={styles.reset_section}></div>
+          <div className={styles.reset_section}>
+            <div className={styles.reset_button} onClick={() => resetOnClick()}>
+              <img src={resetIcon} className={styles.reset_icon} />
+              <span className={styles.reset_text}>Reset</span>
+            </div>
+          </div>
         </div>
         <div className={styles.game_body}>
-          <TicTacToe className={styles.game} />
+          <TicTacToe key={resetKey} className={styles.game} />
         </div>
       </div>
     </>
